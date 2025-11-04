cdfb3f1e93321fecbcd051b6cdd34e96d0cac6f0
trmtanjil <trmtanjil02@gmail.com>
2025-11-04 15:27:13 +0600

    3-1 Stateless vs Stateful

---
+++ level-2moddule-3/3-1StatelessvsStateful.js
@@ -0,0 +1,20 @@
+const count = (couns)=>{
+   let item = 0;
+    item = item + couns
+    return couns
+}
+console.log(count(3))
+console.log(count(4))
+
+const counter = {
+    count :0,
+    add(ammount){
+        this.count = this.count+ammount;
+    },
+    print(){
+        console.log(this.count)
+    },
+}
+counter.add(2);
+counter.add(3)
+counter.print();
