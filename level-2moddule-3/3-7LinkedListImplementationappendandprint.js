16cd5497b36015505e3ea0e7ce21d1c3dc8e6196
trmtanjil <trmtanjil02@gmail.com>
2025-11-09 20:46:23 +0600

    3-8 Linked List Implementation - prepend()

--- level-2moddule-3/3-7LinkedListImplementationappendandprint.js
+++ level-2moddule-3/3-7LinkedListImplementationappendandprint.js
@@ -28,9 +28,48 @@ class Node {
         this.length++;
     }
     
-    prepend(){}
+    prepend(value){
+        const newNode = new Node(value)
 
-    insert(){}
+        //if the linked list is empty
+        if(this.value ===null){
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else{
+            //if the linked list is not empty
+           newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length ++;
+    }
+
+    insert(index, value){
+        if(index <0 || index > this.length){
+            console.error('index out of bound')
+            return undefined
+        }
+        // if the insert is in the start of the linked list 
+        if(index === 0 ){
+            return this.prepend(value)
+        }
+        // if the insert is in the end of the list 
+        if (index === this.length){
+            return this.append(value)
+        }
+        // if the insert is in the middle
+
+        // find the leading node 
+
+        let count = 0;
+        let leadingNode = this.head;
+
+        while(count !== index - 1){
+            leadingNode = leadingNode.next;
+            count++;
+        }
+        console.log(leadingNode)
+    }
 
     remove(){}
 
@@ -53,4 +92,10 @@ class Node {
   likedList.append(2)
    likedList.append(3)
 
-   likedList.print()
+   likedList.prepend(10)
+      likedList.prepend(20)
+       likedList.prepend(30)
+       
+    // likedList.insert(2,100)
+       likedList.print()
+ 
